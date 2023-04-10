0d9f81329583495f2d2b9250d99f61b1bde333e1
MYMioMio <1030708179@qq.com>
2023-04-10 20:50:44 +0800

    Changes

--- dgut/src/Test.java
+++ dgut/src/Test.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 class TCPServer {
-    private static final int PORT = 10000;
+    private static final int PORT = 10006;
     public void listen() throws Exception {
         ServerSocket serverSocket = new ServerSocket(PORT);
         Socket client = serverSocket.accept();
@@ -15,19 +15,35 @@ class TCPServer {
     }
 }
 class TCPClient {
-    private static final int PORT = 10000;
+    private static final int PORT = 10006;
     public void connect() throws Exception {
         Socket client = new Socket(InetAddress.getLocalHost(), PORT);
         byte[] bytes = new byte[1024];
         InputStream is = client.getInputStream();
-
+        int len = is.read(bytes);
+        System.out.println(new String(bytes, 0, len));
+        is.close();
+        client.close();
+    }
+}
+class clientThread implements Runnable {
+    @Override
+    public void run() {
+        TCPClient client = new TCPClient();
+        try {
+            client.connect();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 public class Test {
-    public static void main                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 (String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String str = scan.next();
-        String line = new String(str.getBytes(), 0, str.getBytes().length);
-        System.out.println(line);
+    public static void main(String[] args) throws Exception {
+        clientThread run = new clientThread();
+        Thread client = new Thread(run);
+        client.start();
+        TCPServer server = new TCPServer();
+        server.listen();
+
     }
 }
